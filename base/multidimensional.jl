2de8bb8c4b0e11afe4d85ef7a0e54e1647bc206b
Keno Fischer <keno@alumni.harvard.edu>
2018-01-07 22:25:07 +0100

    Fix regex, tuple, (abstract|offset)array tests

--- base/multidimensional.jl
+++ base/multidimensional.jl
@@ -144,8 +144,8 @@ module IteratorsMD
 
     # nextind with CartesianIndex
     function Base.nextind(a::AbstractArray{<:Any,N}, i::CartesianIndex{N}) where {N}
-        _, ni = next(CartesianIndices(axes(a)), i)
-        return ni
+        ind, _ = iterate(CartesianIndices(axes(a)), i)
+        return ind
     end
 
     # Iteration over the elements of CartesianIndex cannot be supported until its length can be inferred,
